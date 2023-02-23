b295aa45a8dae796d377032b4a920e26796fb17b
S-Gilk <sgilk7@hotmail.com>
2023-02-23 17:13:20 +0000

    Added shell script for datalayer client container build

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 TARGET_ARCH=$1
 echo TARGET_ARCH: ${TARGET_ARCH}
-echo --- build content
-bash build_content.sh ${TARGET_ARCH}
+echo --- build mosquitto
+bash build_mosquitto.sh ${TARGET_ARCH}
+echo --- build datalayer plc client
+bash build_datalayer_mqtt_plc.sh ${TARGET_ARCH}
 echo --- build snap
 bash build_snap.sh ${TARGET_ARCH}
