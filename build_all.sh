34dec55ec7ea9d68010b65aab5de7ad95af1fa71
S-Gilk <sgilk7@hotmail.com>
2023-02-24 16:55:10 +0000

    Docker support for datalayer interface container

--- build_all.sh
+++ build_all.sh
@@ -4,6 +4,6 @@ echo TARGET_ARCH: ${TARGET_ARCH}
 echo --- build mosquitto
 bash build_mosquitto.sh ${TARGET_ARCH}
 echo --- build datalayer plc client
-bash build_datalayer_mqtt_plc.sh ${TARGET_ARCH}
+bash build_ctrlx_datalayer_mqtt_plc.sh ${TARGET_ARCH}
 echo --- build snap
 bash build_snap.sh ${TARGET_ARCH}
---
+++ docker_clean.sh
@@ -0,0 +1,3 @@
+docker container prune -f
+docker image prune -a -f
+docker build -t test .
