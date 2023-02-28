42849f43f5b1f9d0e15c26eb019e34e63c32df53
S-Gilk <sgilk7@hotmail.com>
2023-02-28 22:34:04 +0000

    Added provider node and datalayer subs to specified nodes

--- ctrlx-datalayer-mqtt-interface/main.py
+++ ctrlx-datalayer-mqtt-interface/main.py
@@ -25,42 +25,266 @@
 import sys
 
 import subprocess
+import time
+
+import typing
+from datetime import datetime
 
 from comm.datalayer import Metadata
 
 import ctrlxdatalayer
-from ctrlxdatalayer.variant import Result, VariantType
+from ctrlxdatalayer.variant import Result, Variant, VariantType
 
-from helper.ctrlx_datalayer_helper import get_client, connection_ip
+import ctrlxdatalayer.subscription
+from ctrlxdatalayer.client import Client
+
+from helper.ctrlx_datalayer_helper import get_client, connection_ip, get_provider
+from alldataprovider.nodeManagerAllData import NodeManagerAllData
+
+# addresses of provided values
+address_base = "ctrlx-datalayer-mqtt-interface/"
+datalayer_system = ctrlxdatalayer.system.System("")
+update_flag = False
+address_input = ""
 
 
 def main():
-    with ctrlxdatalayer.system.System("") as datalayer_system:
+    print("Running datalayer interface...")
+    with datalayer_system:
         datalayer_system.start(False)
 
         datalayer_client, datalayer_client_connection_string = get_client(
             datalayer_system)
+
         if datalayer_client is None:
             print("ERROR Connecting", datalayer_client_connection_string, "failed.")
             sys.exit(1)
 
-        with datalayer_client:  # datalayer_client is closed automatically when leaving with block
+        provider, connection_string = get_provider(datalayer_system)
+        if provider is None:
+            print("ERROR Connecting", connection_string, "failed.")
+            datalayer_system.stop(False)
+            sys.exit(1)
 
-            # If not connected exit and retry with app daemon restart-delay (see snapcraft.yaml)
-            if datalayer_client.is_connected() is False:
+        with provider:
+            # Provide datalayer node to set MQTT tree
+            nodeManager = NodeManagerAllData(provider, address_base)
+            data = Variant()
+            data.set_string("")
+            node_address = address_base + "MQTT_Root"
+            nodeManager.create_single_node(addressBranch=node_address, addressType="string", name="",
+                                           unit="string", description="Path to MQTT root", dynamic=True, data=data)
 
-                print("ERROR Not connected to", datalayer_client_connection_string,
-                      "- restarting app after a delay of 10 s ...")
-                sys.exit(2)
+            with datalayer_client:  # datalayer_client is closed automatically when leaving with block
+                # If not connected exit and retry with app daemon restart-delay (see snapcraft.yaml)
+                if datalayer_client.is_connected() is False:
 
-            # Browse the whole ctrlX Data Layer tree
-            print("Browsing and reading nodes...")
-            browse_tree(datalayer_client, datalayer_system.json_converter())
+                    print("ERROR Not connected to", datalayer_client_connection_string,
+                          "- restarting app after a delay of 10 s ...")
+                    sys.exit(2)
+
+                # Subscribe to MQTT root datalayer node
+                subscription_properties = ctrlxdatalayer.subscription.create_properties(
+                    "datalayer-client-sub", publish_interval=100)
+                if subscription_properties is None:
+                    print("ERROR create_properties() returned: None")
+                    sys.exit(1)
+
+                with subscription_properties:
+                    print("Creating subscription to MQTT root node...")
+                    result, subscription = subscribe_single(
+                        datalayer_client, subscription_properties, node_address, rncb_root)
+
+                    if result != Result.OK:
+                        print("ERROR subscribe_single() failed with:", result)
+                        sys.exit(1)
+
+                    if subscription is None:
+                        print("ERROR subscribe_single() returned None")
+                        sys.exit(1)
+
+                    with subscription:
+                        # Endless loop
+                        while datalayer_client.is_connected():
+                            global update_flag
+                            global address_input
+                            if update_flag:
+                                update_flag = False
+                                if address_input != "":
+                                    browse_tree(
+                                        datalayer_client, datalayer_system.json_converter(), address_input)
+                            time.sleep(5.0)
+
+                        subscription.unsubscribe_all()
+                        provider.stop()
 
         print("Stopping Datalayer System")
         # Attention: Doesn't return if any provider or client instance is still running
         stop_ok = datalayer_system.stop(False)
         print("System Stop", stop_ok)
+
+
+def rncb_root(result: Result, items: typing.List[ctrlxdatalayer.subscription.NotifyItem], userdata: ctrlxdatalayer.clib.userData_c_void_p):
+    now = datetime.now().time()
+    print(now, "----------------------------------------------------------")
+    print("ResponseNotifyCallback", result)
+
+    if result != Result.OK:
+        return
+
+    if items is None:
+        print("No items")
+        return
+
+    print("Number of items", len(items))
+    if len(items) <= 0:
+        return
+
+    n = 1
+    for item in items:
+        print("#", n)
+        print("  address:", item.get_address())
+        print("  type:", item.get_data().get_type())
+        print("  value:", item.get_data().get_string())
+        global address_input
+        address_input = item.get_data().get_string()
+        global update_flag
+        update_flag = True
+        print("  timestamp:", item.get_timestamp())
+        print("  datetime:", ctrlxdatalayer.subscription.to_datetime(
+            item.get_timestamp()))
+        n = n + 1
+
+
+def rncb_node(result: Result, items: typing.List[ctrlxdatalayer.subscription.NotifyItem], userdata: ctrlxdatalayer.clib.userData_c_void_p):
+    now = datetime.now().time()
+    print(now, "----------------------------------------------------------")
+    print("ResponseNotifyCallback", result)
+
+    if result != Result.OK:
+        return
+
+    if items is None:
+        print("No items")
+        return
+
+    print("Number of items", len(items))
+    if len(items) <= 0:
+        return
+
+    n = 1
+    for item in items:
+        print("#", n)
+        address = item.get_address()
+        print("  address:", address)
+        vt = item.get_data().get_type()
+        print("  type:", vt)
+        data = item.get_data()
+        value = None
+        if vt == VariantType.ARRAY_BOOL8:
+            value = data.get_array_bool8()
+
+        if vt == VariantType.ARRAY_FLOAT32:
+            value = data.get_array_float32()
+
+        if vt == VariantType.ARRAY_FLOAT64:
+            value = data.get_array_float64()
+
+        if vt == VariantType.ARRAY_INT16:
+            value = data.get_array_int16()
+
+        if vt == VariantType.ARRAY_INT32:
+            value = data.get_array_int32()
+
+        if vt == VariantType.ARRAY_INT64:
+            value = data.get_array_int64()
+
+        if vt == VariantType.ARRAY_INT8:
+            value = data.get_array_int8()
+
+        if vt == VariantType.ARRAY_STRING:
+            value = data.get_array_string()
+
+        if vt == VariantType.ARRAY_UINT16:
+            value = data.get_array_uint16()
+
+        if vt == VariantType.ARRAY_UINT32:
+            value = data.get_array_uint32()
+
+        if vt == VariantType.ARRAY_UINT64:
+            value = data.get_array_uint64()
+
+        if vt == VariantType.ARRAY_UINT8:
+            value = data.get_array_uint8()
+
+        if vt == VariantType.BOOL8:
+            value = data.get_bool8()
+
+        if vt == VariantType.FLOAT32:
+            value = data.get_float32()
+
+        if vt == VariantType.FLOAT64:
+            value = data.get_float64()
+
+        if vt == VariantType.INT16:
+            value = data.get_int16()
+
+        if vt == VariantType.INT32:
+            value = data.get_int32()
+
+        if vt == VariantType.INT64:
+            value = data.get_int64()
+
+        if vt == VariantType.INT8:
+            value = data.get_int8()
+
+        if vt == VariantType.STRING:
+            value = data.get_string()
+
+        if vt == VariantType.UINT16:
+            value = data.get_uint16()
+
+        if vt == VariantType.UINT32:
+            value = data.get_uint32()
+
+        if vt == VariantType.UINT64:
+            value = data.get_uint64()
+
+        if vt == VariantType.UINT8:
+            value = data.get_uint8()
+
+        if value is None:
+            print("WARNING Unknown Variant Type:", vt)
+        else:
+            print("  value:", value)
+
+        if address != "":
+            subprocess.run(["/app/mosquitto_publish.sh",
+                            connection_ip, address, str(value)])
+
+        print("  timestamp:", item.get_timestamp())
+        print("  datetime:", ctrlxdatalayer.subscription.to_datetime(
+            item.get_timestamp()))
+        n = n + 1
+
+
+def subscribe_single(client: Client, subscription_properties: Variant, address: str, callback):
+
+    print("subscribe_single() +++++++++++++++++++++++++++++++++++++++++++++++")
+
+    result, subscription = client.create_subscription_sync(
+        subscription_properties, callback)
+    if result != Result.OK:
+        print("ERROR create_subscription_sync() failed with:", result)
+        return result, None
+
+    if subscription is None:
+        print("ERROR create_subscription_sync() returned: None")
+        return Result.CREATION_FAILED, None
+
+    result = subscription.subscribe(address)
+
+    return result, subscription
 
 
 def browse_tree(client: ctrlxdatalayer.client.Client, converter: ctrlxdatalayer.system.Converter, address="fieldbuses/ethercat/master/instances/ethercatmaster/realtime_data"):
@@ -73,8 +297,21 @@ def browse_tree(client: ctrlxdatalayer.client.Client, converter: ctrlxdatalayer.
     else:
         print(address, node_value)
         if type(node_value) is not list:
+            # Publish updated nodes to MQTT broker
             subprocess.run(["/app/mosquitto_publish.sh",
                             connection_ip, address, str(node_value)])
+            # Subscribe to new topics on datalayer
+            subscription_properties = ctrlxdatalayer.subscription.create_properties(
+                address, publish_interval=100)
+            result, subscription = subscribe_single(
+                client, subscription_properties, address, rncb_node)
+            if result != Result.OK:
+                print("ERROR subscribe_single() failed with:", result)
+                sys.exit(1)
+
+            if subscription is None:
+                print("ERROR subscribe_single() returned None")
+                sys.exit(1)
 
     # Browse Data Layer tree
     result, data = client.browse_sync(address)
