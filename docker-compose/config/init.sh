449d2ad8c01701f349915c8b475f94a4e0ea1ca7
S-Gilk <sgilk7@hotmail.com>
2023-06-22 17:02:53 +0000

    Revert "Initial commit"
    This reverts commit fa429146cca8b16c36960ed66db4e34730cb7fd7.

--- docker-compose/config/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo copying sme.txt to mount directory
-cp /etc/config/sme.txt /etc/sitemanager
-
-echo running SiteManager Embedded
-/sitemanager
---
+++ scripts/build_mosquitto.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+TARGET_ARCH=$1
+echo TARGET_ARCH: ${TARGET_ARCH}
+IMAGE_NAME="eclipse-mosquitto"
+IMAGE_TAG="latest"
+DOCKER_CLI="/snap/bin/docker"
+echo --- create ../docker-compose/docker-compose.env
+rm -v -f ../docker-compose/docker-compose.env
+echo IMAGE_NAME=${IMAGE_NAME} >> ../docker-compose/docker-compose.env
+echo IMAGE_TAG=${IMAGE_TAG} >> ../docker-compose/docker-compose.env
+echo --- create docker image with platform ${TARGET_ARCH}
+rm -f -v ../docker-compose/*.tar
+${DOCKER_CLI} pull ${IMAGE_NAME}:${IMAGE_TAG} --platform ${TARGET_ARCH}
+${DOCKER_CLI} save ${IMAGE_NAME}:${IMAGE_TAG} | gzip > ../docker-compose/image.tar.gz
+${DOCKER_CLI} rmi ${IMAGE_NAME}:${IMAGE_TAG}
